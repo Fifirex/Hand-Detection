6184c80f24548336ccac7e2e86d0c5ff5e237c61
Yash Saraswat <75205458+Fifirex@users.noreply.github.com>
2021-06-18 17:49:01 +0530

    Fixing PR

--- Final.py
+++ Final.py
@@ -1,15 +1,16 @@
 #! /usr/bin/python3
 import cv2
 import mediapipe as mp
-import time
-import numpy as np
 import rospy
 from std_msgs.msg import Int16MultiArray
 
 def talker():
   mp_drawing = mp.solutions.drawing_utils
   mp_hands = mp.solutions.hands
-   
+
+  # FPS counter
+  # last_frame_time, current_frame_time = 0, 0
+
   # Master counter
   i=1 
 
@@ -20,7 +21,7 @@ def talker():
   CONFIG_CYCLE = 50 #def = 50
   ORIENT_CAP = 150  #def = 150
   STRENGTH = 2	    #def = 2
-  
+
   # FPS counter
   # last_frame_time, current_frame_time = 0, 0
 
@@ -40,31 +41,31 @@ def talker():
 
   # For webcam input:
   cap = cv2.VideoCapture(0)
-  
+
   with mp_hands.Hands(
     min_detection_confidence = 0.5,
     max_num_hands = 1,
     min_tracking_confidence = 0.5) as hands:
-    
+
     while cap.isOpened():
       success, image = cap.read()
-      
+
       # init for size of the frame
       h, w, r = image.shape
 
       # DEBUG
       print (arr)
-      
+
       if rospy.is_shutdown():
         exit()
-      
+
       print("Loop Running")
       Md = Int16MultiArray()
       x = [arr[0][0], arr[1][0], arr[0][1], arr[1][1]]
       Md.data = x
       pub.publish(Md)
       rate.sleep()
-      
+
       if not success:
         print("Ignoring empty camera frame.")
         # If loading a video, use 'break' instead of 'continue'.
@@ -100,7 +101,7 @@ def talker():
               if ((ctr >= STRENGTH*CONFIG_CYCLE) and (switch == 0)):
                 arr[1][1] = 1
                 switch = 1
-     
+
             #HANDS_BACK
             if (switch == 1):
               arr[1][1] = 1
@@ -118,7 +119,7 @@ def talker():
                 arr[0][0] = 1
               elif ((id == 4) and (lms.x*w < 2*w//5) and (arr[0][0] != -1)):
                 arr[0][0] = -1
-    
+
             # ROTATION
             if (id == 4):
               low2 = lms.x*w
@@ -161,14 +162,14 @@ def talker():
 
           mp_drawing.draw_landmarks(image, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         i += 1
-   
+
       # HANDS AWAY
       else: 
         arr[0][0] = 0
         arr[1][0] = 0
         arr[0][1] = 0
         arr[1][1] = 0
-    
+
       #cv2.putText(image, fps, (20, 120), font, 3, (10, 155, 0), 3, cv2.LINE_AA)    ## FPS counter
       cv2.line(image, (2 * w // 5, 0), (2 * w // 5, h), (0,0,0), 5)
       cv2.line(image, (3 * w // 5, 0), (3 * w // 5, h), (0,0,0), 5)
